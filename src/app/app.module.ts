414f9ff25b2b163f708e585095246513d1269bca
Ezgi Kütük <ezgi.kutuk@derinarge.com>
2020-01-10 14:46:11 +0300

    ürünlere ihtiyacımız olacağı için product nesnesini içeri import ediyoruz.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import {ProductService} from './product.service';
+import { CartComponent } from './cart/cart.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductComponent
+    ProductComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,

bc475fa288b208c196e2961d2f2a43ce52708512
Ezgi Kütük <ezgi.kutuk@derinarge.com>
2020-01-10 15:41:40 +0300

    Çağırdığımız CartService nesnesini cartService değişkenine atıyoruz. Artık servisimizi istediğimiz yerde kullanabiliriz.   product nesnesini addToCard’atıyoruz. Bu yöntemle ürünü gönderip ürünün sepete eklenmesini işlemini sağlamış oluyoruz.

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { ProductService } from '../product.service';
+import {Component, OnInit} from '@angular/core';
+import {ProductService} from '../product.service';
 import {Product} from './product';
+import {CartService} from '../cart.service';
 
 @Component({
   selector: 'app-product',
@@ -9,15 +10,22 @@ import {Product} from './product';
 })
 export class ProductComponent implements OnInit {
   products: Product[];
+
   constructor(
-    private  productService: ProductService
-  ) { }
-productResult: any;
+    private  productService: ProductService,
+    private  cartService: CartService
+  ) {
+  }
+
+  productResult: any;
   loading: boolean = true;
   addedProduct: string;
+
   addToCard(product: Product) {
     this.addedProduct = product.productName;
+    this.cartService.addToCart(product);
   }
+
   ngOnInit(): void {
     this.productService.getProduct().subscribe(response => {
       this.productResult = response;
